2a919a96fd5b78a5d359ba0a2cacb6a2b00ac9bf
Vladimir Suragin <v.suragin@yclients.tech>
2021-10-04 12:14:39 +0300

    use cli tools

---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+namespace Hexlet\Code\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greet()
+{
+  line('Welcome to the Brain Game!');
+  $name = prompt('May I have your name?');
+  line("Hello, %s!", $name);
+}
