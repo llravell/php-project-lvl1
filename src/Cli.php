d34b558fd24c67b6b6d8424511d84891770212ea
Vladimir Suragin <v.suragin@yclients.tech>
2021-10-04 18:24:37 +0300

    add lint

--- src/Cli.php
+++ src/Cli.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Hexlet\Code\Cli;
 
 use function cli\line;
@@ -6,7 +7,7 @@ use function cli\prompt;
 
 function greet()
 {
-  line('Welcome to the Brain Game!');
-  $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
